54ea5cbce4622a0c2a7309dab9722003b79cbdc1
rasitkuscu <iamrasit@gmail.com>
2016-08-30 21:13:31 +0300

    removed build folder

--- build/lib/angular2-contextmenu/angular2-contextmenu.d.ts
+++
@@ -1,7 +0,0 @@
-import { ContextMenuComponent } from './src/contextMenu.component';
-export * from './src/contextMenu.component';
-export * from './src/contextMenu.service';
-declare var _default: {
-    directives: typeof ContextMenuComponent[];
-};
-export default _default;
--- build/lib/angular2-moment/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './CalendarPipe';
-export * from './DateFormatPipe';
-export * from './DurationPipe';
-export * from './FromUnixPipe';
-export * from './TimeAgoPipe';
-export * from './DifferencePipe';
